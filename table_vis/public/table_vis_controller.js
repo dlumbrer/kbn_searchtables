08c7975aa1e703baccc0c33b5d55e4369aa84866
David Moreno <dmorenolumb@gmail.com>
2017-05-26 19:01:57 +0200

    Added files and first tests

---
+++ table_vis/public/table_vis_controller.js
@@ -0,0 +1,72 @@
+import AggResponseTabifyTabifyProvider from 'ui/agg_response/tabify/tabify';
+import uiModules from 'ui/modules';
+import { assign } from 'lodash';
+
+// get the kibana/table_vis module, and make sure that it requires the "kibana" module if it
+// didn't already
+const module = uiModules.get('kibana/table_vis', ['kibana']);
+
+// add a controller to tha module, which will transform the esResponse into a
+// tabular format that we can pass to the table directive
+module.controller('KbnTableVisController', function ($scope, $element, Private) {
+  const tabifyAggResponse = Private(AggResponseTabifyTabifyProvider);
+
+  const uiStateSort = ($scope.uiState) ? $scope.uiState.get('vis.params.sort') : {};
+  assign($scope.vis.params.sort, uiStateSort);
+
+  $scope.sort = $scope.vis.params.sort;
+  $scope.$watchCollection('sort', function (newSort) {
+    $scope.uiState.set('vis.params.sort', newSort);
+  });
+
+  /**
+   * Recreate the entire table when:
+   * - the underlying data changes (esResponse)
+   * - one of the view options changes (vis.params)
+   */
+  $scope.$watchMulti(['esResponse', 'vis.params', 'inputSearch'], function ([resp]) {
+
+    let tableGroups = $scope.tableGroups = null;
+    let hasSomeRows = $scope.hasSomeRows = null;
+
+    if (resp) {
+      const vis = $scope.vis;
+      const params = vis.params;
+
+      $scope.inputSearch = "functest"
+
+      tableGroups = tabifyAggResponse(vis, resp, {
+        partialRows: params.showPartialRows,
+        minimalColumns: vis.isHierarchical() && !params.showMeticsAtAllLevels,
+        asAggConfigResults: true
+      });
+
+      hasSomeRows = tableGroups.tables.some(function haveRows(table) {
+        if (table.tables) return table.tables.some(haveRows);
+        return table.rows.length > 0;
+      });
+
+      $element.trigger('renderComplete');
+    }
+
+    $scope.hasSomeRows = hasSomeRows;
+    if (hasSomeRows) {
+
+      var newrows = []
+
+      for (var i = 0; i < tableGroups.tables[0].rows.length; i++) {
+        for (var j = 0; j < tableGroups.tables[0].rows[i].length; j++) {
+          if(typeof tableGroups.tables[0].rows[i][j].key === 'string'){
+            if(tableGroups.tables[0].rows[i][j].key.includes($scope.inputSearch)){
+              newrows.push(tableGroups.tables[0].rows[i])
+              break;
+            }
+          }
+        }
+      }
+      tableGroups.tables[0].rows = newrows;
+      console.log(newrows)
+      $scope.tableGroups = tableGroups;
+    }
+  });
+});
